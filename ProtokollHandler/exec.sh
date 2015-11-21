ebd4ee66495d96a7ecd076e9bd4183ef579623ec
Eng958 <dieter.engemann@gmail.com>
2015-11-21 21:23:55 +0100

    Automatische Suche der verknuepften Applikation

--- ProtokollHandler/exec.sh
+++ ProtokollHandler/exec.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
+#-----------------------------------------------------
+# Execution-Script als Protokoll-Handler
+#
+# 21.11.2015	v0.1	Rudimentaeres Script als Test
+#-----------------------------------------------------
 file=file:///home/dieter/Musik/Musik-Wiki/Lehrbuecher/JameyAbersold/Volume06-All-Bird/02-NowsTheTime-BilliesBounce/02-NowsTheTime-BilliesBounce.mp3 
+file=file:///home/dieter/Musik/Musik-Wiki/Lehrbuecher/JameyAbersold/Volume06-All-Bird/02-NowsTheTime-BilliesBounce/02-NowsTheTime-BilliesBounce.pdf 
 
-protocol=$(echo $1 | cut -d : -f 1)
-address=$(echo $1 | cut -d / -f 3)
-user=$(echo $address | grep @ | cut -d @ -f 1)
-port=$(echo $address | grep : | cut -d : -f 2)
-host=$(echo $address | cut -d @ -f 2 | cut -d : -f 1)
+file=$1
 
 extension=$(echo $file | cut -d . -f 2)
 filename=$(echo $file | cut -d : -f 2)
 
-echo $extension
-echo $filename
+echo "Extension: "$extension
+echo "Filename: "$filename
 
-echo $1
+
+
+desktopFile=$(grep $extension /usr/share/applications/defaults.list | cut -d = -f 2 | uniq | cut -d . -f 1)
+echo $desktopFile
+
+###grep Exec /usr/share/applications/$desktopFile
+
 sleep 2
-clementine $filename &
+$desktopFile $filename &
 
 
 case $extension in
 mp3)
-connectstring=$(echo "$([ -z $user ] || echo "$user@")$host$([ -z $port ] || echo "-p $port")")
-gnome-terminal --window -e "$protocol $connectstring"
+	echo "Running MP3"
 ;;
-telnet)
-connectstring=$(echo "$host $([ -z $port ] || echo "$port")$([ -z $user ] || echo "-l $user")")
-gnome-terminal --window -e "$protocol $connectstring"
-;;
-rdesktop)
-connectstring=$(echo "$([ -z $user ] || echo "-u $user ")$host$([ -z $port ] || echo ":$port")")
-rdesktop -g 1280x960 $connectstring
-;;
-vnc)
-connectstring=$(echo "$host$([ -z $port ] || echo":$port")")
-vncviewer $connectstring
+pdf)
+	echo "Running PDF"
 ;;
 esac
