f5afd26c58129222ca42455c65533fbd5eccb7e4
Eng958 <dieter.engemann@gmail.com>
2015-11-22 21:40:25 +0100

    Ausgaben in Lofile; Weitere Anpassungen und Verbesserungen

--- ProtokollHandler/exec.sh
+++ ProtokollHandler/exec.sh
@@ -3,11 +3,15 @@
 # Execution-Script als Protokoll-Handler
 #
 # 21.11.2015	v0.1	Rudimentaeres Script als Test
+# 22.11.2015	v0.2	Output in Logfile
 #-----------------------------------------------------
-file=file:///home/dieter/Musik/Musik-Wiki/Lehrbuecher/JameyAbersold/Volume06-All-Bird/02-NowsTheTime-BilliesBounce/02-NowsTheTime-BilliesBounce.mp3 
-file=file:///home/dieter/Musik/Musik-Wiki/Lehrbuecher/JameyAbersold/Volume06-All-Bird/02-NowsTheTime-BilliesBounce/02-NowsTheTime-BilliesBounce.pdf 
 
+logfile=/tmp/exec.log 
+exec > $logfile 
+
+date
 file=$1
+echo $file
 
 extension=$(echo $file | cut -d . -f 2)
 filename=$(echo $file | cut -d : -f 2)
@@ -15,22 +19,10 @@ filename=$(echo $file | cut -d : -f 2)
 echo "Extension: "$extension
 echo "Filename: "$filename
 
-
+zenity --error --text "$filename"
 
 desktopFile=$(grep $extension /usr/share/applications/defaults.list | cut -d = -f 2 | uniq | cut -d . -f 1)
 echo $desktopFile
 
-###grep Exec /usr/share/applications/$desktopFile
-
-sleep 2
 $desktopFile $filename &
 
-
-case $extension in
-mp3)
-	echo "Running MP3"
-;;
-pdf)
-	echo "Running PDF"
-;;
-esac
